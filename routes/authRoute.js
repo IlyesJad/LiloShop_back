308b50902841b7f7588aaee03764ca743371092f
unknown <ilyes.jad@esprit.tn>
2023-05-23 01:39:21 +0100

    Liloshopv2

---
+++ routes/authRoute.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn,isSeller } from "../middlewares/authMiddlewares.js";
+
+//router object
+const router = express.Router();
+
+//routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+//seller routes
+router.get("/seller",isSeller,testController)
+
+export default router;
